442b89437e015f325e896c8628a447ec43e139b7
mahk3235 <mahk3235@gmail.com>
2024-07-12 19:02:35 +0530

    basic function

---
+++ basic function.js
@@ -0,0 +1,21 @@
+//simple function :
+
+function sum(x,y){
+    return x + y;
+}
+
+ /*
+ 1)this code define the fun sum 
+ 2)that means you can call sum(1,3) which return 4.
+   sum(3,4) return 7.
+3)in javascript you have to return from inside function .if you forget to write return ,
+4)your function will return undefine .
+5)return also quit or exit function .
+
+function sum(x,y){
+return x+ y;
+console.log("Hello world")----console.log never run 
+}
+
+bec return quit or exit funciton 
+*/
